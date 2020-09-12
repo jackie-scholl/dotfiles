82f54cc4bfd054e460af53dbb9e3eeee13d816fd
Jackie Scholl <jackie.h.scholl@gmail.com>
2020-09-12 17:35:16 -0400

    woops, forgot the layout

---
+++ .xmonad/lib/Layout.hs
@@ -0,0 +1,28 @@
+
+{-# LANGUAGE FlexibleContexts #-}
+
+module Layout (myLayouts) where
+
+import XMonad
+
+import GHC.Word
+
+import XMonad.Layout.Spacing
+import XMonad.Layout.Spiral
+import XMonad.Layout.PerWorkspace --(onWorkspace)
+--import XMonad.Layout.NoBorders
+import XMonad.Hooks.ManageDocks
+
+import XMonad.Layout.LayoutModifier (ModifiedLayout)
+
+mySpacing = spacingRaw True emptyBorder False smallBorder True
+    where
+        emptyBorder = Border 0 0 0 0
+        smallBorder = Border 5 5 5 5
+        largeBorder = Border 30 30 30 30
+        
+myLayouts = mySpacing $ myLayouts2
+
+myLayouts2 :: PerWorkspace Tall (PerWorkspace (Mirror Tall) XMonad.Layout.Spiral.SpiralWithDir) GHC.Word.Word64
+myLayouts2 = onWorkspace "left" (Tall 1 (3/100) (1/3)) $ onWorkspace "right" (Mirror $ Tall 1 (3/100) (3/5)) $ spiral (6/7)
+
