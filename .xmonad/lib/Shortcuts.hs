2a55da172910602b45228b11f1770c1bb95fb09b
Jackie Scholl <jackie.h.scholl@gmail.com>
2020-09-12 16:01:25 -0400

    separate out xmonad shortcuts into a library

---
+++ .xmonad/lib/Shortcuts.hs
@@ -0,0 +1,131 @@
+{-# LANGUAGE FlexibleContexts, ScopedTypeVariables #-}
+
+module Shortcuts (myShortcuts) where
+
+import XMonad
+
+import qualified XMonad.StackSet as W
+import qualified XMonad.Util.EZConfig as EZConfig
+--import XMonad.Util.Dzen
+import qualified XMonad.Util.Paste as Paste
+
+import qualified Data.List as List 
+import Data.Foldable (fold)
+import qualified Data.Map.Strict as M
+import qualified Data.Set as S
+import Data.Maybe
+import Control.Monad
+import System.IO
+
+type Keystroke = (KeyMask, KeySym)
+
+myShortcuts :: XConfig l -> M.Map Keystroke (X ())
+myShortcuts config = windowingKeys config <> audioKeys config <> myShortcutMaps
+
+windowingKeys :: XConfig l -> M.Map Keystroke (X ())
+windowingKeys config = includeModifier `M.mapKeys` (focusSpaceAndWindow <$> indexAllKeys myWorkspaceKeyMapping)
+    where
+        myWorkspaceKeyMapping :: [(String, [KeySym])]
+        myWorkspaceKeyMapping = [("left", [xK_1..xK_5]), ("right", [xK_6..xK_9])]
+
+        indexList :: (Ord a) => [a] -> M.Map a Int
+        indexList xs = M.fromList $ zip xs [0..] 
+        
+        indexKeys :: (String, [KeySym]) -> M.Map KeySym (String, Int)
+        indexKeys (x, y) = pair x <$> indexList y
+
+        indexAllKeys :: [(String, [KeySym])] -> M.Map KeySym (String, Int)
+        indexAllKeys workspaceKeyMapping = fold $ indexKeys <$> workspaceKeyMapping
+
+        focusSpaceAndWindow :: (String, Int) -> X ()
+        focusSpaceAndWindow (workspaceName, windowNumber) = windows $ focusSpaceAndWindowHelper workspaceName windowNumber
+
+        includeModifier :: KeySym -> Keystroke
+        includeModifier = pair (XMonad.modMask config)
+        
+        focusSpaceAndWindowHelper :: String -> Int -> WindowSet -> WindowSet
+        focusSpaceAndWindowHelper myWorkspace windowNumber windowSet = focused
+            where
+                correctedWorkspace = W.view myWorkspace windowSet
+                windowList = W.index correctedWorkspace
+                targetWindow = windowList !! windowNumber
+                focused = W.focusWindow targetWindow correctedWorkspace
+
+audioKeys :: XConfig l -> M.Map Keystroke (X ())
+audioKeys config = EZConfig.mkKeymap config [
+    ("<XF86AudioMute>",        spawn "amixer -q  set Master mute"),
+    ("<XF86AudioLowerVolume>", spawn "amixer -q sset Master 2%-"),
+    ("<XF86AudioRaiseVolume>", spawn "amixer -q sset Master 2%+")
+    ]
+
+
+
+
+sendKey2 :: Keystroke -> X ()
+sendKey2 = uncurry Paste.sendKey
+
+shortcutMapper :: M.Map String (M.Map Keystroke Keystroke) -> M.Map Keystroke (X ())
+shortcutMapper shortcutMap = M.mapWithKey sendMappedKeystroke $ invertMapOfMaps shortcutMap
+
+sendMappedKeystroke :: Keystroke -> M.Map String Keystroke -> X ()
+sendMappedKeystroke ks destMap = getKeystroke >>= sendKey2 -- produceOriginalInput sendKey2 >>= showKey
+    where
+        getKeystroke :: X Keystroke
+        getKeystroke = fromMaybe ks <$> possibleKeystroke
+
+        possibleKeystroke :: X (Maybe Keystroke)
+        possibleKeystroke = withFocused' $ runQuery mappedQuery
+
+        mappedQuery :: Query (Maybe Keystroke)
+        mappedQuery = (destMap M.!?) <$> className
+
+withFocused' :: forall a. (Window -> X (Maybe a)) -> X (Maybe a)
+withFocused' f = withWindowSet $ runOnFocusedWindow
+    where
+        runOnFocusedWindow :: WindowSet -> X (Maybe a)
+        runOnFocusedWindow ws = fromMaybe (pure Nothing) $ f <$> W.peek ws
+
+myShortcutMaps :: M.Map Keystroke (X ())
+myShortcutMaps = shortcutMapper $ M.fromList
+    [   ("fakeProgramDoesnExist", M.empty)
+        , ("Firefox", firefoxShortcuts)
+    ]
+
+
+firefoxShortcuts :: M.Map Keystroke Keystroke
+firefoxShortcuts = M.fromList $ [
+    ((mod1Mask, xK_Right), (controlMask, xK_Page_Down)),
+    ((mod1Mask, xK_Left ), (controlMask, xK_Page_Up  )),
+    ((controlMask, xK_Q),  (noModMask, xK_VoidSymbol))
+    ]
+
+--withMask :: KeyMask -> Keystroke -> Keystroke
+--withMask addMask (keyMask, keySym) = (keyMask .|. addMask, keySym)
+
+
+-- UTILITIES
+
+pair :: a -> b -> (a, b)
+pair a b = (a, b)
+
+produceOriginalInput :: Functor f => (a -> f ()) -> (a -> f a)
+produceOriginalInput action input = fmap (const input) (action input)
+
+both :: (a -> b) -> (a, a) -> (b, b)
+both f (x, y) = (f x, f y) 
+
+invertMapOfMaps :: forall a b c. (Ord a, Ord b) => M.Map a (M.Map b c) -> M.Map b (M.Map a c)
+invertMapOfMaps myMap = M.fromSet bToACMap bset
+    where
+        bset :: S.Set b
+        bset = fold $ fmap M.keysSet myMap
+
+        bToACMap :: b -> M.Map a c
+        bToACMap bravo = M.mapMaybe (M.lookup bravo) myMap
+
+
+
+
+
+
+
--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -6,39 +6,27 @@ import XMonad.Config.Kde
 
 import XMonad.Layout.Grid
 import XMonad.Layout.MultiColumns
-import XMonad.Layout.ThreeColumns
 import XMonad.Layout.Spacing
 import XMonad.Layout.NoBorders
 import XMonad.Layout.LayoutModifier (ModifiedLayout)
     
-import qualified XMonad.StackSet as W
-
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
 import XMonad.ManageHook
 import XMonad.Operations
-import XMonad.Util.Dzen
+--import XMonad.Util.Dzen
 import XMonad.Util.EZConfig
-import XMonad.Util.NamedWindows
-import qualified XMonad.Util.Paste as Paste
-import XMonad.Util.Run (spawnPipe)
 
-import Data.Foldable (fold)
-import qualified Data.Map.Strict as M
-import qualified Data.Set as S
-import Data.Maybe
-import Control.Monad
-import System.IO
+import Shortcuts
 
-main = xmonad =<< (myStatusBar $ docks myConfig )--`additionalKeysP` myKeymap {-`additionalKeys` myShortcutMaps1-} )
+main = xmonad =<< (myStatusBar $ docks myConfig )
 
 myConfig = def
         { manageHook = manageHook kde4Config <+> manageDocks <+> (isFullscreen --> doFullFloat) <+> manageHook def
         , layoutHook = myLayouts
-        , keys = \c -> (M.singleton (mod4Mask, xK_Left) $ Paste.sendKey controlMask xK_Page_Up) <> myShortcutMaps1 <> mkKeymap c myKeymap <> keys defaultConfig c
-        --, keys = \c -> mkKeymap c myKeymap
-        , startupHook = return () >> checkKeymap myConfig myKeymap >> blah2 >> (Paste.sendKey controlMask xK_Page_Up)
+        , keys = \c ->  myShortcuts c <> keys defaultConfig c
+        --, startupHook = return () >> checkKeymap myConfig audioKeys
         , handleEventHook = handleEventHook defaultConfig <+> docksEventHook
         , terminal = "konsole"
         , focusFollowsMouse = False
@@ -46,139 +34,23 @@ myConfig = def
         , modMask = mod4Mask     -- Rebind Mod to the Windows key
         }
 
-blah2 :: X ()
-blah2 = myDzen "foobar" 
-
-myDzen :: String -> X ()
-myDzen = dzenConfig myDzenConfig
-    where
-        myDzenConfig :: DzenConfig
-        myDzenConfig = center 200 200 0
-
 mySpacing = spacingRaw True emptyBorder False smallBorder True
     where
         emptyBorder = Border 0 0 0 0
         smallBorder = Border 5 5 5 5
         largeBorder = Border 30 30 30 30
         
-myLayouts = (noBorders $ Full) ||| 
-            (avoidStruts $ mySpacing $ 
-            Tall 1 (3/100) (1/2) ||| Mirror (Tall 1 (3/100) (3/5)))
+myLayouts = avoidStruts $ mySpacing $ 
+            Tall 1 (3/100) (1/3) ||| Mirror (Tall 1 (3/100) (3/5))
 
 myPrettyPrinter = xmobarPP
-                { ppCurrent = xmobarColor "blue" "" . wrap "[" "]"
-                -- , ppVisible = const ""
-                -- , ppLayout = const ""
-                , ppTitle =  xmobarColor "green" "" . shorten 50
+                { ppTitle =  xmobarColor "green" "" . shorten 50
                 , ppOrder = myPPOrder
                 }
-
-myPPOrder (a:b:c:x) = [c]
+    where
+        myPPOrder (a:b:c:x) = [c]
 
 myStatusBar = statusBar "xmobar" myPrettyPrinter $ const (noModMask, xK_VoidSymbol)
 
-myKeymap :: [(String, X ())]
-myKeymap = windowingKeys <> audioKeys -- <> firefoxKeys 
-
-windowingKeys :: [(String, X ())]
-windowingKeys = [("M-" ++ show (modifier windowNum), helper workspace windowNum)
-          | windowNum <- [0..4],
-          (workspace, modifier) <- [("left", \x -> x + 1), ("right", \x -> (x+6) `mod` 10) ]
-          ]
-    where
-        helper myWorkspace windowNumber = windows $ \x -> helper2 x myWorkspace windowNumber 
-
-        helper2 :: WindowSet -> String -> Int -> WindowSet
-        helper2 windowSet myWorkspace windowNumber = focused
-            where
-                correctedWorkspace :: WindowSet
-                correctedWorkspace = W.view myWorkspace windowSet
-                windowList = W.index correctedWorkspace
-                targetWindow = windowList !! windowNumber
-                focused = W.focusWindow targetWindow correctedWorkspace 
-
-audioKeys :: [(String, X ())]
-audioKeys = [
-    ("<XF86AudioMute>",        spawn "amixer -q  set Master mute"),
-    ("<XF86AudioLowerVolume>", spawn "amixer -q sset Master 2%-"),
-    ("<XF86AudioRaiseVolume>", spawn "amixer -q sset Master 2%+")
-    ]
 
 
-
-
-invertMapOfMaps :: forall a b c. (Ord a, Ord b) => M.Map a (M.Map b c) -> M.Map b (M.Map a c)
-invertMapOfMaps myMap = M.fromSet bToACMap bset
-    where
-        bset :: S.Set b
-        bset = fold $ fmap M.keysSet myMap
-
-        bToACMap :: b -> M.Map a c
-        bToACMap bravo = M.mapMaybe (M.lookup bravo) myMap
-
-type Keystroke = (KeyMask, KeySym)
-sendKey2 :: Keystroke -> X ()
-sendKey2 = uncurry Paste.sendKey
-
-showKey :: Keystroke -> X ()
-showKey = myDzen . show
-
-produceOriginalInput :: Functor f => (a -> f ()) -> (a -> f a)
-produceOriginalInput action input = fmap (const input) (action input)
-
-shortcutMapper :: M.Map String (M.Map Keystroke Keystroke) -> M.Map Keystroke (X ())
-shortcutMapper shortcutMap = M.mapWithKey sendMappedKeystroke $ invertMapOfMaps shortcutMap
-
-sendMappedKeystroke :: Keystroke -> M.Map String Keystroke -> X ()
-sendMappedKeystroke ks destMap = getKeystroke >>= produceOriginalInput sendKey2 >>= showKey
-    where
-        getKeystroke :: X Keystroke
-        getKeystroke = fromMaybe ks <$> possibleKeystroke
-
-        possibleKeystroke :: X (Maybe Keystroke)
-        possibleKeystroke = withFocused' $ runQuery mappedQuery
-
-        mappedQuery :: Query (Maybe Keystroke)
-        mappedQuery = (destMap M.!?) <$> className
-
-withFocused' :: forall a. (Window -> X (Maybe a)) -> X (Maybe a)
-withFocused' f = withWindowSet $ runOnFocusedWindow
-    where
-        runOnFocusedWindow :: WindowSet -> X (Maybe a)
-        runOnFocusedWindow ws = fromMaybe (pure Nothing) $ f <$> W.peek ws
-
-myShortcutMaps :: M.Map String (M.Map Keystroke Keystroke)
-myShortcutMaps = M.fromList
-    [
-        ("Firefox", firefoxShortcuts)
-        , ("konsole", firefoxShortcuts)
-    ]
-
-myShortcutMaps1 :: M.Map Keystroke (X ())
-myShortcutMaps1 = shortcutMapper myShortcutMaps
-
-
-firefoxShortcuts :: M.Map Keystroke Keystroke
-firefoxShortcuts = M.fromList $ [
-    ((mod1Mask, xK_Right), (controlMask, xK_Page_Down)),
-    ((mod1Mask, xK_Left ), (controlMask, xK_Page_Up  ))
-    ]
-
-withMask :: KeyMask -> Keystroke -> Keystroke
-withMask addMask (keyMask, keySym) = (keyMask .|. addMask, keySym)
-
-
-{-}
-firefoxKeys :: [(String, X ())]
-firefoxKeys = [
-    ("M1-<Right>", queryAndChangeTab MRight),
-    ("M1-<Left>",  queryAndChangeTab MLeft )
-    ]
--}
-
-        {-, logHook = dynamicLogWithPP xmobarPP
-                        { ppCurrent = xmobarColor "blue" "" . wrap "[" "]"
-                        , ppTitle = xmobarColor "green" "" . shorten 50
-                        , ppOutput = hPutStrLn xmproc
-                        }
-        -}
