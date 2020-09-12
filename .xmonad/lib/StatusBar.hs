0e1b0ac6b703f45e12344d3b10ab0b1757f3102d
Jackie Scholl <jackie.h.scholl@gmail.com>
2020-09-12 17:34:45 -0400

    xmonad: separate layout, futz with status bar

--- .xmonad/lib/StatusBar.hs
+++ .xmonad/lib/StatusBar.hs
@@ -4,6 +4,8 @@ module StatusBar (myPrettyPrinter) where
 
 import XMonad
 import XMonad.Util.Dzen
+import XMonad.Util.Loggers
+
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
@@ -14,9 +16,17 @@ myPrettyPrinter :: PP
 myPrettyPrinter = dzenPP
                 { ppTitle =  dzenColor "green" "" . shorten 50
                 --, ppOrder = myPPOrder
+                , ppSep = " | "
+                , ppExtras = [date dateFormat, loadAvg, logCmd volumeCommand, logCmd "echo hello"]
                 }
     where
         myPPOrder (a:b:c:x) = [c]
 
+dateFormat :: String
+dateFormat = "%a %b %_d %Y %-I:%M:%S %p"
+
+volumeCommand = "amixer sget Master | egrep --only-matching --max-count 1 '([0-9]){2,3}%'"
+
 
 --myStatusBar = statusBar "dzen2" myPrettyPrinter $ const (noModMask, xK_VoidSymbol)
+
--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -20,14 +20,12 @@ import XMonad.Util.EZConfig
 
 import Shortcuts
 import StatusBar
+import Layout
 
 main = xmonad =<< (myStatusBar $ docks myConfig )
 
-myDzenFlags :: String 
-myDzenFlags = "-e onstart lower -w 800 -h 100"
-
 myConfig = def
-        { manageHook = manageHook kde4Config {-<+> manageDocks <+> (isFullscreen --> doFullFloat)-} <+> manageHook def
+        { manageHook = {-manageDocks <+> (isFullscreen --> doFullFloat) <+>-} manageHook def
         , layoutHook = myLayouts
         , keys = \c ->  myShortcuts c <> keys defaultConfig c
         --, startupHook = return () >> checkKeymap myConfig audioKeys
@@ -38,17 +36,9 @@ myConfig = def
         , modMask = mod4Mask     -- Rebind Mod to the Windows key
         }
 
-mySpacing = spacingRaw True emptyBorder False smallBorder True
-    where
-        emptyBorder = Border 0 0 0 0
-        smallBorder = Border 5 5 5 5
-        largeBorder = Border 30 30 30 30
-        
-myLayouts = avoidStruts $ mySpacing $ 
-            Tall 1 (3/100) (1/3) ||| Mirror (Tall 1 (3/100) (3/5))
+myStatusBar = statusBar "xmobar" myXmobarPP $ const (noModMask, xK_VoidSymbol)
 
+myXmobarPP = xmobarPP {ppTitle = xmobarColor "green" "" . shorten 50, ppOrder = third}
 
-myStatusBar = statusBar "dzen2" myPrettyPrinter $ const (noModMask, xK_VoidSymbol)
-
-
+third (a:b:c:x) = [c]
 
