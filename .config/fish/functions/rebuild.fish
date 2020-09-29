63a9c8d4725c51bab7a46046210b4a859d3cc16e
Jackie Scholl <jackie.h.scholl@gmail.com>
2020-09-29 11:10:23 -0400

    nix upgrade on rebuild by default

--- .config/fish/functions/rebuild.fish
+++ .config/fish/functions/rebuild.fish
@@ -1,6 +1,6 @@
 # Defined in /var/folders/dm/2kd_hgp51qx5v41_4ct5c8tw001d24/T//fish.pNd6bl/rebuild.fish @ line 2
 function rebuild --description 'Rebuilds the current NixOS configuration.' 
     pushd /etc/nixos
-    sudo sh -c "time nixos-rebuild switch $argv"
+    sudo sh -c "time nixos-rebuild switch --upgrade $argv"
     popd
 end
