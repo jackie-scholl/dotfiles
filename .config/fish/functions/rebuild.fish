83b8c995bbc15347b68aa8eb6cbbc0f43078ec3c
Jackie Scholl <jackie.h.scholl@gmail.com>
2021-05-17 09:58:29 -0400

    turn off GPU lighting on rebuild

--- .config/fish/functions/rebuild.fish
+++ .config/fish/functions/rebuild.fish
@@ -1,5 +1,6 @@
 # Defined in /var/folders/dm/2kd_hgp51qx5v41_4ct5c8tw001d24/T//fish.pNd6bl/rebuild.fish @ line 2
 function rebuild --description 'Rebuilds the current NixOS configuration.' 
+    nvidia-settings --ctrl-display :0 --assign GPULogoBrightness=0
     pushd /etc/nixos
     sudo sh -c "time nixos-rebuild switch --upgrade $argv"
     sudo sh -c "time nix-collect-garbage --delete-older-than 30d"
