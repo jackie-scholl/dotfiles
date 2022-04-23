a85d0e174ef9389c5b701c422ab3337b4b805e3a
Jackie Scholl <jackie.h.scholl@gmail.com>
2022-04-23 12:22:22 -0400

    slight updates

--- .config/fish/functions/rebuild.fish
+++ .config/fish/functions/rebuild.fish
@@ -1,6 +1,7 @@
 # Defined in /var/folders/dm/2kd_hgp51qx5v41_4ct5c8tw001d24/T//fish.pNd6bl/rebuild.fish @ line 2
 function rebuild --description 'Rebuilds the current NixOS configuration.' 
     #pushd /etc/nixos
+    ibus-daemon -d
     sudo sh -c "time nixos-rebuild switch --upgrade $argv" &&
     sudo sh -c "time nix-collect-garbage --delete-older-than 30d"
     #popd
